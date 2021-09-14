7d0b732a0fcffe570b0a31bcbccddbeeec426d8a
endrigo7@gmail.com <endrigo7@gmail.com>
2021-09-14 08:26:35 -0300

    my first test

--- src/test/kotlin/com/school/cesar/cib/util/CPFUtilTest.kt
+++ src/test/kotlin/com/school/cesar/cib/util/CPFUtilTest.kt
@@ -7,10 +7,9 @@ class CPFUtilTest {
 
     @Test
     fun `deve retornar cpf sem mascara quando receber cpf com mascara`(){
-        val retornoEsperado = "02312312312"
+        val retornoEsperado = "12312312312"
         val retornoObitdo = CPFUtil.limpaCPF("123.123.123-12")
 
         assertEquals(retornoEsperado, retornoObitdo)
     }
-
 }
