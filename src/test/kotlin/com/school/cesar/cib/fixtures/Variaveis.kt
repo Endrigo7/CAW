e56b604ab3b4498ffaf7b494b8edb14ddd84e08f
endrigo7@gmail.com <endrigo7@gmail.com>
2021-09-17 09:33:36 -0300

    fixtures

---
+++ src/test/kotlin/com/school/cesar/cib/fixtures/Variaveis.kt
@@ -0,0 +1,11 @@
+package com.school.cesar.cib.fixtures
+
+import com.school.cesar.cib.entity.Aluno
+
+val aluno = Aluno(
+    nome = "Endrigo Ferreira",
+    cpf = "123.123.123-12",
+    senha = "123456778",
+    primeiraSenha = true
+)
+
--- src/test/kotlin/com/school/cesar/cib/validator/AlunoValidatorTest.kt
+++ src/test/kotlin/com/school/cesar/cib/validator/AlunoValidatorTest.kt
@@ -1,7 +1,7 @@
 package com.school.cesar.cib.validator
 
-import com.school.cesar.cib.entity.Aluno
 import com.school.cesar.cib.exception.AlunoInvalidoException
+import com.school.cesar.cib.fixtures.aluno
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -10,18 +10,19 @@ class AlunoValidatorTest {
 
     @Test
     fun `deve lancar excecao quando nome for vazio`() {
-        val aluno = Aluno(
-            nome = "",
-            cpf = "123",
-            senha = "123456778",
-            primeiraSenha = true
-        )
-
         assertThrows<AlunoInvalidoException> {
-            AlunoValidator.validar(aluno)
+            AlunoValidator.validar(aluno.copy(nome = ""))
         }.also {
             assertEquals("O nome deve ser preenchido", it.message)
         }
     }
 
+    @Test
+    fun `deve lancar excecao quando cpf for vazio`() {
+        assertThrows<AlunoInvalidoException> {
+            AlunoValidator.validar(aluno.copy(cpf = ""))
+        }.also {
+            assertEquals("O cpf deve ser preenchido", it.message)
+        }
+    }
 }
